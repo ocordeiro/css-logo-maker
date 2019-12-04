72f9241cd88c9e1e1657564753a4cd3d4136d8da
Alan Cordeiro <alamcordeiro@gmail.com>
2019-12-04 09:36:17 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import { images, path } from './gaming.json'
 
+
+let buildInput = (image, type, value) => {
+    let input = document.createElement('input')
+    input.setAttribute('id', `${type}-${name}`)
+    input.value = value
+    return input
+}
+
+
 for (name in images) {
-    console.log(name)
+
+    let image = images[name]
+
     let img = document.createElement('img')
     img.src = `images/${path}/${name}`
+    img.setAttribute('data-path', name)
+
+    let image_width = buildInput(name, 'width', image.image_width)
+
     img.onclick = (event) => {
-        alert(event.offsetX +','+ event.offsetY)
+        let img_id = event.target.getAttribute('data-path')
+        let img_input = document.getElementById(img_id)
+        img_input.value = event.offsetX + ',' + event.offsetY
     }
     document.getElementById('images').append(img)
+    document.getElementById('images').append(image_width)
 }
+
+
